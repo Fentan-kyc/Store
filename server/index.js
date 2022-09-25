4ff33ec4cbc89efd4dfcdec9d97dd99dc3f3dea2
Fentan-kyc <di-ma02@mail.ru>
2022-09-25 22:42:33 +0300

    json restponse on code 200

--- server/index.js
+++ server/index.js
@@ -2,11 +2,18 @@ import dotenv from 'dotenv/config';
 import express from "express";
 
 import sequelize from './bd.js';
-import models from './models/models.js'
+import cors from 'cors';
+import models from './models/models.js';
 
 const PORT = process.env.PORT || 5000;
 
-const app = express()
+const app = express() 
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (reg, res) => {
+    res.status(200).json({message: "warning!"})
+})
 
 const startDb = async () => {
     try{
