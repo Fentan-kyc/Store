fa3a19837aa26a8c1081326b0d8f905a99f398d5
Fentan-kyc <di-ma02@mail.ru>
2022-10-01 15:09:47 +0300

    ErrorHandlingMiddleware

--- server/index.js
+++ server/index.js
@@ -5,13 +5,17 @@ import sequelize from './bd.js';
 import cors from 'cors';
 import models from './models/models.js';
 import router from './routes/index.js';
+import errorHandler from './middleware/ErrorHandlingMiddleware.js';
 
 const PORT = process.env.PORT || 5000;
 
-const app = express() 
-app.use(cors())
-app.use(express.json())
-app.use('/api', router)
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use('/api', router);
+
+
+app.use(errorHandler); // ever last
 
 const startDb = async () => {
     try{
