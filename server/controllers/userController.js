21ff9565017ff880618a8ec67c9ec8f6c8581164
Fentan-kyc <di-ma02@mail.ru>
2022-10-04 19:39:33 +0300

    registration & authorization

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,21 +1,55 @@
 import ApiError from '../error/ApiError.js';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import {User, Basket} from '../models/models.js';
+import dotenv from 'dotenv/config';
+
+const generateJWT = (id, email, role) => {
+    return jwt.sign({id: id, email, role}, 
+        process.env.SECRET_KEY,
+        {expiresIn: '24h'}
+        );
+}
+
 class UserController{
 
-    async registration(req, res){
-
-    }
-
-    async login(req, res){
-
-    }
-
-    async check(req, res, next){
-        const {id} = req.query;
-
-        if (!id || isNaN(id)){
-            return next(ApiError.badRequest('Unknown id'))
+    async registration(req, res, next){
+        const {email, password, role}  = req.body;
+        if(!email || !password){
+            return next(ApiError.badRequest('Unknown email or password'))
         }
-        res.json(id)
+
+        const candidate = await User.findOne({where:{email}});
+        if(candidate){
+            return next(ApiError.badRequest('User already exist'))
+        }
+
+        const hasPassword = await bcrypt.hash(password, 5);
+        const user = await User.create({email,password: hasPassword ,role})
+        const basket = await Basket.create({userId:user.id})
+        const token = generateJWT(user.id, user.email, user.role)
+        return res.json({token})
+    }
+
+    async login(req, res, next){
+        const {email, password} = req.body;
+        
+        const candidate = await User.findOne({where:{email}})
+        if(!candidate){
+            return next(ApiError.internal("Unknown user"))
+        }
+
+        let comparePassword = bcrypt.compareSync(password, candidate.password);
+        if(!comparePassword){
+            return next(ApiError.internal("Wrong password!"))
+        }
+
+        const token = generateJWT(candidate.id, candidate.email, candidate.role)
+        return res.json(token);
+    }
+
+    async check(){
+
     }
 
 }
